9436fd701422eff01e2ec374e1da46876af2f2a5
dulith <dulithcsenanayake@gmail.com>
2022-11-09 14:54:40 +0530

    Add test cases for smb2 scenarios

---
+++ integration/mediation-tests/tests-patches/src/test/java/org/wso2/carbon/esb/vfs/transport/test/connection/failure/SMB2SFileTransferTestCase.java
@@ -0,0 +1,201 @@
+/*
+* Copyright (c) 2022, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+*
+* WSO2 Inc. licenses this file to you under the Apache License,
+* Version 2.0 (the "License"); you may not use this file except
+* in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing,
+* software distributed under the License is distributed on an
+* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+* KIND, either express or implied. See the License for the
+* specific language governing permissions and limitations
+* under the License.
+*/
+package org.wso2.carbon.esb.vfs.transport.test.connection.failure;
+
+import org.apache.axiom.om.OMElement;
+import org.apache.axiom.om.util.AXIOMUtil;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.awaitility.Awaitility;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import org.wso2.esb.integration.common.utils.ESBIntegrationTest;
+import org.wso2.esb.integration.common.utils.common.ServerConfigurationManager;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.channels.FileChannel;
+import java.util.concurrent.Callable;
+import java.util.concurrent.TimeUnit;
+import javax.xml.stream.XMLStreamException;
+
+/**
+ * Integration test for https://github.com/wso2/product-ei/issues/5456
+ */
+public class SMB2SFileTransferTestCase extends ESBIntegrationTest {
+
+    private static final Log LOGGER = LogFactory.getLog(SMB2SFileTransferTestCase.class);
+
+    private File inputFolder;
+    private File outputFolder;
+    private ServerConfigurationManager serverConfigurationManager;
+    private String inputFolderName = "in";
+    private String outputFolderName = "out";
+    // Need to read from env variables for support jenkins
+    private static final String USERNAME = "malaka";
+    private static final String PASSWORD = "malaka";
+
+    @BeforeClass(alwaysRun = true)
+    public void checkRootFolder() throws Exception {
+
+        //Need to read from env variables
+        String pathToFtpDir = "/home/malaka/MyFolder/esb_patch_14/SMB2Test/test";
+
+        // Local folder of the SMB2 server root
+        File FTPFolder = new File(pathToFtpDir);
+        Assert.assertTrue(FTPFolder.exists(), "SMB2 root file folder not created");
+
+        // define a directory under SMB2 server root
+        inputFolder = new File(FTPFolder.getAbsolutePath() + File.separator + inputFolderName);
+        // define a directory under SMB2 server root
+        outputFolder = new File(FTPFolder.getAbsolutePath() + File.separator + outputFolderName);
+
+        Assert.assertTrue(inputFolder.exists(), "FTP data /in folder not created");
+        Assert.assertTrue(outputFolder.exists(), "FTP data /out folder not created");
+
+        super.init();
+        //Not needed for this draft test
+        /*File jksFile = new File(getClass().getResource("/artifacts/ESB/synapseconfig/vfsTransport"
+                                                       + "/ESBJAVA4770/vfsKeystore.jks").getPath());
+        File destinationJks = Paths.get(CarbonBaseUtils.getCarbonHome(),"repository"
+                                                 ,"resources","security","vfsKeystore.jks").toFile();
+        //copy keystore which contains key with keystrength 2048
+        copyFile(jksFile, destinationJks);*/
+
+        // replace the axis2.xml enabled vfs transfer and restart the ESB server gracefully.
+        serverConfigurationManager = new ServerConfigurationManager(context);
+        serverConfigurationManager.applyConfiguration(
+                new File(getClass().getResource("/artifacts/ESB/synapseconfig/"
+                                                + "vfsTransport/ESBJAVA4770/axis2.xml").getPath()));
+        super.init();
+    }
+
+
+    @Test(groups = "wso2.esb", description = "SMB2 file transfer test")
+    public void fileTransferTest() throws XMLStreamException, IOException {
+
+
+        // Still hard coded need to be read from env variables
+
+        String fileUri =
+                "smb2://malaka:malaka@localhost/share/in";
+        String moveAfterProcess =
+                "smb2://malaka:malaka@localhost/share/out";
+
+        //create VFS transport SMB2 listener proxy (need to improve with param support)
+        String proxy = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+                "<proxy xmlns=\"http://ws.apache.org/ns/synapse\"\n" +
+                "       name=\"Polling_Test\"\n" +
+                "       transports=\"vfs\"\n" +
+                "       startOnLoad=\"true\">\n" +
+                "   <target>\n" +
+                "      <inSequence>\n" +
+                "         <log level=\"custom\">\n" +
+                "            <property name=\"Polling_Test\"\n" +
+                "                      expression=\"fn:concat($ctx:proxy.name,' file ',$trp:FILE_NAME,' received from file system')\"/>\n" +
+                "         </log>\n" +
+                "      </inSequence>\n" +
+                "      <faultSequence><!-- set $trp:ERROR_CODE to keep source untouched -->\n" +
+                "         <property name=\"ERROR_CODE\" value=\"1\" scope=\"transport\"/>\n" +
+                "         <!-- -->\n" +
+                "         <log level=\"custom\">\n" +
+                "            <property name=\"ERROR in proxy \"\n" +
+                "                      value=\"Polling_Test\"/>\n" +
+                "            <property name=\"code\" expression=\"$ctx:ERROR_CODE\"/>\n" +
+                "            <property name=\"detail\" expression=\"$ctx:ERROR_DETAIL\"/>\n" +
+                "            <property name=\"exception\" expression=\"$ctx:ERROR_EXCEPTION\"/>\n" +
+                "         </log>\n" +
+                "         <!-- -->\n" +
+                "         <property name=\"status\" value=\"Error\"/>\n" +
+                "         <property name=\"errorMessage\" value=\"unable to handle file transfer. Rollback!\"/>\n" +
+                "      </faultSequence>\n" +
+                "   </target>\n" +
+                "   <parameter name=\"transport.PollInterval\">1</parameter>\n" +
+                "   <parameter name=\"transport.vfs.Maxfilesize\">10000000</parameter>\n" +
+                "   <parameter name=\"transport.vfs.FileURI\">smb2://malaka:malaka@localhost/share/in</parameter>\n" +
+                "   <parameter name=\"transport.vfs.ContentType\">text/plain</parameter>\n" +
+                "   <parameter name=\"transport.vfs.MoveAfterProcess\">smb2://malaka:malaka@localhost/share/out</parameter> \n" +
+                "    <parameter name=\"transport.vfs.MoveAfterFailure\">smb2://malaka:malaka@localhost/share/original</parameter>\n" +
+                "    <parameter name=\"transport.vfs.ActionAfterProcess\">MOVE</parameter>\n" +
+                "    <parameter name=\"transport.vfs.ActionAfterFailure\">MOVE</parameter>\n" +
+                "   <parameter name=\"transport.vfs.ClusterAware\">false</parameter>\n" +
+                "   <parameter name=\"transport.vfs.FileNamePattern\">.*\\.txt</parameter>\n" +
+                "   <parameter name=\"transport.vfs.Locking\">disable</parameter>\n" +
+                "</proxy>";
+
+        OMElement proxyOM = AXIOMUtil.stringToOM(proxy);
+
+        //add the listener proxy to ESB server
+        try {
+            addProxyService(proxyOM);
+        } catch (Exception e) {
+            LOGGER.error("Error while updating the Synapse config", e);
+        }
+        LOGGER.info("Synapse config updated");
+        // Here we can't know whether the proxy polling happened or not, hence only way is to wait and see. Since poll interval is 1,
+        // this waiting period should suffice. But it may include the time it take to deploy the service as well.
+        //check whether all 100 files are moved to "out" folder
+        Awaitility.await().atMost(120, TimeUnit.SECONDS).until(checkForOutputFile(outputFolder));
+    }
+
+    /*
+    * This is needed when other test cases are added since we need to copy files in after class method
+    * */
+    /**
+     * Copy the given source file to the given destination
+     *
+     * @param sourceFile source file
+     * @param destFile   destination file
+     * @throws IOException
+     */
+    public static void copyFile(File sourceFile, File destFile) throws IOException {
+        if (!destFile.exists()) {
+            destFile.createNewFile();
+        }
+        FileInputStream fileInputStream = null;
+        FileOutputStream fileOutputStream = null;
+        try {
+            fileInputStream = new FileInputStream(sourceFile);
+            fileOutputStream = new FileOutputStream(destFile);
+
+            FileChannel source = fileInputStream.getChannel();
+            FileChannel destination = fileOutputStream.getChannel();
+            destination.transferFrom(source, 0, source.size());
+        } finally {
+            IOUtils.closeQuietly(fileInputStream);
+            IOUtils.closeQuietly(fileOutputStream);
+        }
+    }
+
+    /*
+    * Check whether all the files have been copied from in to out
+    * */
+    private Callable<Boolean> checkForOutputFile(final File outputFolder) {
+        return new Callable<Boolean>() {
+            @Override
+            public Boolean call() {
+                File[] files = outputFolder.listFiles();
+                return files != null && files.length == 100;
+            }
+        };
+    }
+}
