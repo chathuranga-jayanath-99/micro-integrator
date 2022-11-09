edc5508175ebdb51bcb20d172bc28156a11e2059
dulith <dulithcsenanayake@gmail.com>
2022-11-09 16:28:27 +0530

    Add content check test cases

--- integration/mediation-tests/tests-patches/src/test/java/org/wso2/carbon/esb/vfs/transport/test/connection/failure/Utils.java
+++ integration/mediation-tests/tests-patches/src/test/java/org/wso2/carbon/esb/vfs/transport/test/connection/failure/Utils.java
@@ -23,8 +23,11 @@ import org.apache.commons.logging.LogFactory;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class Utils {
 
@@ -205,5 +208,47 @@ public class Utils {
 
     public static void deleteDirectory(File directory) throws IOException {
         FileUtils.deleteDirectory(directory);
+    }
+
+    /*
+     * this method return the complete hash of the file
+     * */
+    public static String checksum(File file) throws IOException, NoSuchAlgorithmException {
+        // instantiate a MessageDigest Object by passing
+        // string "MD5" this means that this object will use
+        // MD5 hashing algorithm to generate the checksum
+        MessageDigest digest = MessageDigest.getInstance("MD5");
+        // Get file input stream for reading the file content
+        FileInputStream fis = new FileInputStream(file);
+        // Create byte array to read data in chunks
+        byte[] byteArray = new byte[1024];
+        int bytesCount = 0;
+        // read the data from file and update that data in the message digest
+        while ((bytesCount = fis.read(byteArray)) != -1) {
+            digest.update(byteArray, 0, bytesCount);
+        }
+        // close the input stream
+        fis.close();
+        // store the bytes returned by the digest() method
+        byte[] bytes = digest.digest();
+
+        // this array of bytes has bytes in decimal format
+        // so we need to convert it into hexadecimal format
+
+        // for this we create an object of StringBuilder
+        // since it allows us to update the string i.e. its
+        // mutable
+        StringBuilder sb = new StringBuilder();
+
+        // loop through the bytes array
+        for (int i = 0; i < bytes.length; i++) {
+            // the following line converts the decimal into
+            // hexadecimal format and appends that to the
+            // StringBuilder object
+            sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
+        }
+
+        // Finally we return the complete hash
+        return sb.toString();
     }
 }
