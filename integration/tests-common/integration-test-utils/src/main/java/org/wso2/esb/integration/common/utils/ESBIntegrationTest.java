a0b7f1778420fc4097040c43c610b7fa0c524850
dulith <dulithcsenanayake@gmail.com>
2022-11-10 16:04:07 +0530

    Remove additional hostname

--- integration/tests-common/integration-test-utils/src/main/java/org/wso2/esb/integration/common/utils/ESBIntegrationTest.java
+++ integration/tests-common/integration-test-utils/src/main/java/org/wso2/esb/integration/common/utils/ESBIntegrationTest.java
@@ -92,7 +92,6 @@ public abstract class ESBIntegrationTest {
     protected StockQuoteClient axis2Client;
     protected ContextUrls contextUrls = new ContextUrls();
     protected String sessionCookie;
-    protected String hostName = null;
     protected OMElement synapseConfiguration = null;
     protected ESBTestCaseUtils esbUtils;
     protected AutomationContext context;
