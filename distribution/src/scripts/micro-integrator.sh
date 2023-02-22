b91767923b8eaee19f51414fa9db1e2e8703bbad
dhanushka madushan <madushandhanushka@gmail.com>
2023-02-22 11:36:37 +0530

    Introduce Liveness API resource for MI
    Fix https://github.com/wso2/api-manager/issues/1463

--- distribution/src/scripts/micro-integrator.sh
+++ distribution/src/scripts/micro-integrator.sh
@@ -326,6 +326,7 @@ do
     -DavoidConfigHashRead=true \
     -Dproperties.file.path=default \
     -DenableReadinessProbe=true \
+    -DenableLivenessProbe=true \
     -DenableManagementApi=true \
     -Dlog4j2.contextSelector=org.apache.logging.log4j.core.async.AsyncLoggerContextSelector \
     -Dorg.ops4j.pax.logging.logReaderEnabled=false \
