cfe8f9ac007c4ebbacebc3273969387ad5db4f77
chiranSachintha <chiransachintha1994@gmail.com>
2025-11-10 11:18:16 +0530

    Fix review suggestion

--- components/org.wso2.micro.integrator.initializer/src/main/java/org/wso2/micro/integrator/initializer/deployment/synapse/deployer/SynapseAppDeployer.java
+++ components/org.wso2.micro.integrator.initializer/src/main/java/org/wso2/micro/integrator/initializer/deployment/synapse/deployer/SynapseAppDeployer.java
@@ -1324,7 +1324,10 @@ public class SynapseAppDeployer implements AppDeploymentHandler {
 
     private String getConnectorName(String artifactName) {
         final String prefix = "mi-connector-";
-        return artifactName.substring(prefix.length());
+        if (artifactName.startsWith(prefix)) {
+            return artifactName.substring(prefix.length());
+        }
+        return artifactName.substring(0, artifactName.lastIndexOf("-connector"));
     }
 
     private String getTemplateName(String artifactPath, String name) {
